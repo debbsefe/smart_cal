3692d9a9f0e670beef00c0e52ae53cbdade6432b
Eferha Mamus <eferhamamus@gmail.com>
2025-01-02 17:19:52 +0100

    feat: add smart event editor (#11)

---
+++ lib/calendar/notifier/calendar_state.dart
@@ -0,0 +1,38 @@
+part of 'calendar_notifier.dart';
+
+@freezed
+class CalendarState with _$CalendarState {
+  factory CalendarState({
+    required DateTime selectedDate,
+    @Default([]) List<SmartEvent> events,
+  }) = _CalendarState;
+
+  CalendarState._();
+
+  LinkedHashMap<DateTime, List<SmartEvent>> get kEvents =>
+      LinkedHashMap<DateTime, List<SmartEvent>>(
+        equals: isSameDay,
+        hashCode: getHashCode,
+      )..addAll(getEvents());
+
+  Map<DateTime, List<SmartEvent>> getEvents() {
+    final map = <DateTime, List<SmartEvent>>{};
+    for (final event in events) {
+      final key = DateTime.utc(
+        event.date.year,
+        event.date.month,
+        event.date.day,
+      );
+      if (map.containsKey(key)) {
+        map[key]!.add(event);
+      } else {
+        map[key] = [event];
+      }
+    }
+    return map;
+  }
+}
+
+int getHashCode(DateTime key) {
+  return key.day * 1000000 + key.month * 10000 + key.year;
+}
--- lib/core/model/smart_event.dart
+++ lib/core/model/smart_event.dart
@@ -1,37 +1,59 @@
-import 'dart:collection';
-
-import 'package:table_calendar/table_calendar.dart';
+import 'package:flutter/material.dart';
 
 class SmartEvent {
-  const SmartEvent(this.title);
+  const SmartEvent({
+    required this.id,
+    required this.title,
+    required this.date,
+    required this.time,
+    this.description,
+    this.isRecurring = false,
+    this.recurringType,
+    this.adjustBasedOnCompletion = false,
+  });
   final String title;
+  final String? description;
+  final DateTime date;
+  final TimeOfDay time;
+  final bool isRecurring;
+  final RecurringType? recurringType;
+  final bool adjustBasedOnCompletion;
+  final String id;
+
+  SmartEvent copyWith({
+    String? id,
+    String? title,
+    String? description,
+    DateTime? date,
+    TimeOfDay? time,
+    bool? isRecurring,
+    RecurringType? recurringType,
+    bool? adjustBasedOnCompletion,
+  }) {
+    return SmartEvent(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      date: date ?? this.date,
+      time: time ?? this.time,
+      isRecurring: isRecurring ?? this.isRecurring,
+      recurringType: recurringType ?? this.recurringType,
+      adjustBasedOnCompletion:
+          adjustBasedOnCompletion ?? this.adjustBasedOnCompletion,
+    );
+  }
 
   @override
   String toString() => title;
 }
 
-final kEvents = LinkedHashMap<DateTime, List<SmartEvent>>(
-  equals: isSameDay,
-  hashCode: getHashCode,
-)..addAll(_kEventSource);
-
-final _kEventSource = {
-  for (var item in List.generate(50, (index) => index))
-    DateTime.utc(kFirstDay.year, kFirstDay.month, item * 5): List.generate(
-      item % 4 + 1,
-      (index) => SmartEvent('Event $item | ${index + 1}'),
-    ),
-}..addAll({
-    kToday: [
-      const SmartEvent("Today's Event 1"),
-      const SmartEvent("Today's Event 2"),
-    ],
-  });
-
-int getHashCode(DateTime key) {
-  return key.day * 1000000 + key.month * 10000 + key.year;
-}
-
 final kToday = DateTime.now();
 final kFirstDay = DateTime(kToday.year, kToday.month - 3, kToday.day);
 final kLastDay = DateTime(kToday.year, kToday.month + 3, kToday.day);
+
+enum RecurringType {
+  daily,
+  weekly,
+  monthly,
+  yearly,
+}
