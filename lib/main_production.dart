ebbe475b7feaafa797cfb6542a40f6e4c4b9161e
Mamus <eferhamamus@gmail.com>
2025-08-31 21:56:32 +0200

    chore: remove flutter_dotenv dependency and related code from main files

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,10 +1,8 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:smart_cal/app_config.dart';
 import 'package:smart_cal/bootstrap.dart';
 import 'package:smart_cal/features/app/app.dart';
 
 Future<void> main() async {
-  await dotenv.load(isOptional: true);
   await bootstrap(
     (container) => App(container: container),
     AppConfig(),
